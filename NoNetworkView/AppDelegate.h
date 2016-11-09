71b6d41131c24092468445b8ba3164dfdfd8f4be
maxiaogang <15825931829@163.com>
2016-11-09 09:48:21 +0800

    Initial Commit

---
+++ NoNetworkView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NoNetworkView
+//
+//  Created by honyork on 2016/11/9.
+//  Copyright © 2016年 HuaJianCi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
